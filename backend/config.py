c1ee2660376bda17aa8421173fb36adf137a5d1a
Arming926 <kelvin20040926@gmail.com>
2026-03-04 20:16:22 +0800

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-# Flask configuration
-
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
-    JSONIFY_PRETTYPRINT_REGULAR = False
