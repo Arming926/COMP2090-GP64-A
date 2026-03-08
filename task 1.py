5906e97b45ff2d7dd40cd7630c4a9f4b54380009
Arming926 <kelvin20040926@gmail.com>
2026-03-08 18:19:28 +0800

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -5,14 +5,14 @@ class User:
         self.nickname = nickname
         self.group = None
 
-    def get_nickname(self):
-        return self.nickname
-
     def get_username(self):
         return self.username
 
     def check_password(self, pwd):
         return self.password == pwd
+
+    def get_nickname(self):
+        return self.nickname
 
     def set_group(self, group):
         self.group = group
@@ -25,3 +25,4 @@ class User:
             return f"{self.nickname}   Still Empty..."
         else:
             return f"{self.nickname}   {self.group.get_name()}   {self.group.get_current()}/{self.__group.get_limit()}"
+
